dc01cbca526508e006a6faf44e66bdd6e4f113af
SaNi191 <143741958+SaNi191@users.noreply.github.com>
2025-05-01 00:09:32 -0600

    set up basic flask app

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # modules
 import json
 import numpy as np
-from flask import Flask
+from website import init_app
 
+if __name__ == '__main__':
+    web_app = init_app()
+    web_app.run(debug=True)
 
 for i in range(10):
     print(i)
--- website/__init__.py
+++ website/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 
 def init_app():
     app = Flask(__name__)
+    
     return app
 
 
