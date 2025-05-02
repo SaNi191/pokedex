aaeb3d30c6ebfa303d6dee37089be5c154b24cfb
SaNi191 <143741958+SaNi191@users.noreply.github.com>
2025-05-02 12:42:08 -0600

    added more routes

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from website import app
 if __name__ == '__main__':
     app.run(debug=True)
 
+#requests.get("")
 
 for i in range(10):
     print(i)
--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,23 @@
 #__init__.py indicates directory is a python package
 from flask import Flask
 
-app = Flask(__name__)
-
+app = Flask(
+    __name__, 
+    template_folder='templates'
+)
 
 
 @app.route('/')
 def home():
     return "<h1>Hello World!</h1>"
+
+@app.route('/rankings')
+def rankings():
+    return "<h1>Rankings Here!</h1>"
+
+@app.route('/pokedex')
+def pokedex():
+    return "<h1>PokeDex here</h1>"
 
 
 
