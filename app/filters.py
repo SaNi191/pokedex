1a36d45b05f89d45f555b2ae85f6519ec0b0c0d6
SaNi191 <143741958+SaNi191@users.noreply.github.com>
2025-05-09 18:18:22 -0600

    added image to dex page, fetches dex url using filter, pokeapi fetching and caching

--- app/filters.py
+++ app/filters.py
@@ -1,12 +1,47 @@
-from . import app
-from app.utils.pokeapi import get_ability
+import os
+import requests
 
+from . import app
+from app.utils.pokeapi import get_ability, get_sprite
+
+
+# filter to return pokemon ability information from pokeapi
 @app.template_filter('poke_ability')
 def poke_ability(txt, selector):
     result = get_ability(txt)
     if not result:
         return 'not found'
     return result[selector]
+
+# filter to return pokemon image address from static folder
+# use pokeapi to pull image if not found
+@app.template_filter('image_path')
+def image_path(txt):
+
+    # create images folder if it doesn't exist
+    if not os.path.exists(os.path.join(app.static_folder, 'images')):
+        os.makedirs(os.path.join(app.static_folder, 'images'))
+
+    # check if image exists in static folder
+    img_path = os.path.join(app.static_folder, 'images', f'{txt}.png')
+
+
+    if os.path.exists(img_path):
+        return f'images/{txt}.png'
+    
+    
+    # retrieve image from pokeapi
+    response = get_sprite(txt)
+    
+    if not response:
+        return None
+    
+    # save image to static folder
+    with open(img_path, 'wb') as f:
+        f.write(response.content)
+
+    return f'images/{txt}.png'
+
 
 # testing a filter
 @app.template_filter('caesar_shift')
--- app/utils/pokeapi.py
+++ app/utils/pokeapi.py
@@ -3,38 +3,42 @@ import requests
 POKEAPI_URL = 'https://pokeapi.co/api/v2/'
 
 # api_request(endpoint) will attempt to query pokeapi for desired endpoint, returning False if failed
-def api_request(endpoint):
+def api_request(endpoint, jsonify = False):
     try:
         response = requests.get(endpoint, timeout = 2)
         response.raise_for_status()
-        return response.json()
+        if jsonify:
+            return response.json()
+        else:
+            return response
     except (requests.HTTPError, TimeoutError) as e:
         print(e)
         return False
 
-
+# get_pokemon(name) will call api_request to get the pokemon data from pokeapi
 def get_pokemon(name):
     # call api_request, noting that if any erroneous response occurred the retval will be false
-    return api_request(f'{POKEAPI_URL}pokemon/{name}')
+    return api_request(f'{POKEAPI_URL}pokemon/{name}', True)
 
+# get_stats(name) will call api_request to get the pokemon stats from pokeapi, returning a dictionary of stats
 def get_stats(name):
 
-    result = api_request(f'{POKEAPI_URL}pokemon/{name}')
+    result = api_request(f'{POKEAPI_URL}pokemon/{name}', True)
 
     if result:
         return {stat['stat']['name']: stat['base_stat'] for stat in result['stats']}
     else:
         return result
 
-
+# get_ability(name) will call api_request to get the pokemon ability from pokeapi, returning a list of [name, effect]
 def get_ability(name):
-    result = api_request(f'{POKEAPI_URL}pokemon/{name}')
+    result = api_request(f'{POKEAPI_URL}pokemon/{name}', True)
 
     if not result:
         return result
 
 
-    ability_result = api_request(result['abilities'][0]['ability']['url'])
+    ability_result = api_request(result['abilities'][0]['ability']['url'], True)
     if not ability_result:
         return ability_result
 
@@ -48,3 +52,19 @@ def get_ability(name):
         ability_txt = 'Ability was not found'
     
     return [ability_result['name'], ability_txt]
+
+# get_sprite(name) will call api_request to get the pokemon sprite from pokeapi, returning a response object
+def get_sprite(name):
+
+    result = api_request(f'{POKEAPI_URL}pokemon/{name}', True)
+
+    if not result:
+        return result
+
+    sprite_url = result['sprites']['front_default']
+
+    response = api_request(sprite_url, False)
+    if not response:
+        return False
+    
+    return response
